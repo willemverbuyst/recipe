f091213e172103b463841881c98000b7883855bf
Willem Verbuyst <willemverbuyst@gmail.com>
2023-10-22 07:15:29 +0200

    Add basic navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  loadedFeature = 'recipe';
   title = 'recipe';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
